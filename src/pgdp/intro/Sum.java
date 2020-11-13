3887243a9872a256b5555eafc5e8b64ecd3ba049
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2020-11-13 12:07:45 +0100

    TUTU

--- src/pgdp/intro/Sum.java
+++ src/pgdp/intro/Sum.java
@@ -6,6 +6,13 @@ public class Sum {
 
 	public static void main(String[] args) {
 
-	}
+		int x = -1;
+		int sum = 0;
 
+		while(x!=0) {
+				x = readInt("Bitte Zahl eingeben:");
+				sum += x;
+		}
+		write("Summe:\n"+ sum);
+	}
 }
