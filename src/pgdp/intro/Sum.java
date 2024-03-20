3ba418ac57c3aa6416fe39732392cda6f242be74
tuksa12 <rapha.tuksa@hotmail.com>
2024-03-20 18:18:05 +0100

    TUTU

--- src/pgdp/intro/Sum.java
+++ src/pgdp/intro/Sum.java
@@ -6,13 +6,34 @@ public class Sum {
 
 	public static void main(String[] args) {
 
-		int x = -1;
-		int sum = 0;
+		//int x = -1;
+		//int sum = 0;
 
-		while(x!=0) {
-				x = readInt("Bitte Zahl eingeben:");
-				sum += x;
-		}
-		write("Summe:\n"+ sum);
+		//while(x!=0) {
+				//x = readInt("Bitte Zahl eingeben:");
+				//sum += x;
+		//}
+		//write("Summe:\n"+ sum);
+
+
+
+		//Kontrollflussdiagram
+		int n, p, s, q;
+		n = readInt();
+		p = readInt();
+		s = 100;
+		q = 0;
+		while (s > 0) {
+			if (n < p * s) {
+				write(q);
+				q = 0;
+				s = s / 10;
+				}
+			else {
+				n = n - p * s;
+				q = q + 1;
+				}
+			}
+		write(n);
 	}
 }
